6963cadc3f26bdc0eec9bcf15a5bb3d88b3ecd4e
Just-do-it-LB <60215541+Just-do-it-LB@users.noreply.github.com>
2022-12-27 15:52:46 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# tdesign-Table
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-rysblz)
---
+++ src/demo.vue
@@ -0,0 +1,67 @@
+<template>
+  <t-space direction="vertical">
+    <!-- 当数据为空需要占位时，会显示 cellEmptyContent -->
+    <t-table
+      rowKey="index"
+      :data="data"
+      :columns="columns"
+      :stripe="stripe"
+      :bordered="bordered"
+      :hover="hover"
+      :size="size"
+      :table-layout="tableLayout ? 'auto' : 'fixed'"
+      :pagination="pagination"
+      :showHeader="showHeader"
+      cellEmptyContent="-"
+    ></t-table>
+  </t-space>
+</template>
+<script lang="jsx">
+const data = [];
+const total = 15;
+for (let i = 0; i < total; i++) {
+  data.push({
+    index: i,
+    platform: i % 2 === 0 ? '共有' : '私有',
+    type: ['String', 'Number', 'Array', 'Object'][i % 4],
+  });
+}
+export default {
+  data() {
+    return {
+      data,
+      size: 'medium',
+      tableLayout: false,
+      stripe: true,
+      bordered: true,
+      hover: false,
+      showHeader: true,
+
+      columns: [
+        {
+          // 序号列，设置 colKey = serial-number 即可
+          colKey: 'serial-number',
+          title: '序号',
+          width: 100,
+        },
+        {
+          width: 100,
+          colKey: 'platform',
+          title: '平台',
+        },
+        {
+          colKey: 'type',
+          title: '类型',
+          width: 100,
+        },
+      ],
+      /** 非受控用法：与分页组件对齐 */
+      pagination: {
+        current: 2,
+        defaultPageSize: 5,
+        total,
+      },
+    };
+  },
+};
+</script>
